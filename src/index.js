5e50e664cf5f496d387925f61460f3c0c4f3353b
Yury Samsonenko <cant09y@gmail.com>
2023-10-12 20:53:41 +0300

    feat: do 18 on 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  console.log(str);
+  console.log(bracketsConfig);
+
+  const OPEN_BRACKETS = bracketsConfig.map((el) => el[0]);
+  const CLOSE_BRACKETS = bracketsConfig.map((el) => el[1]);
+  const BRACKETS_PAIR = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    BRACKETS_PAIR[`${CLOSE_BRACKETS[i]}`] = OPEN_BRACKETS[i];
+  }
+
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let curSymbol = str[i];
+
+    if (
+      curSymbol === '|' &&
+      str.slice(0, i).replace(/[^|]/g, '').length % 2 === 0
+    ) {
+      stack.push(curSymbol);
+    } else if (OPEN_BRACKETS.includes(curSymbol) && curSymbol !== '|') {
+      stack.push(curSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      const topElement = stack[stack.length - 1];
+
+      if (BRACKETS_PAIR[curSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
